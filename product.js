885060f212fe54c1d2022a5743a31ce1dc8d0d5e
shahul <you@example.com>
2024-06-18 20:22:55 +0530

    added features serach

--- product.js
+++ product.js
@@ -6,3 +6,26 @@ menuicon.addEventListener("click",function(){
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+///search funtionall
+var productcontainer = document.getElementById("product_container")
+var search = document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div")
+
+
+
+  search .addEventListener("keyup",function(){
+    var enteredValue =event.target.value.toUpperCase()
+
+    for (count = 0;count<productlist.length;count=count+1)
+        {
+            var productname = productlist[count].querySelector("h1").textContent
+            if(productname.toUpperCase().indexOf(enteredValue)<0)
+                {
+                    productlist[count].style.display="none"
+
+                }
+            else{
+                productlist[count].style.display="block"
+            }
+        }
+  })
